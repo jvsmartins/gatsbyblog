828afc48a3cb4d452e04ef33ecaf4d359be6ed4b
Joao Vitor Martins <j.martins@aluno.ifsp.edu.br>
2021-02-14 21:12:51 -0300

    Estilização do componente Avatar

---
+++ src/components/SocialLinks/Icons.js
@@ -0,0 +1,11 @@
+import { Github } from "@styled-icons/entypo-social/Github"
+import { Twitter } from "@styled-icons/entypo-social/Twitter"
+import { Instagram } from "@styled-icons/entypo-social/Instagram"
+
+const Icons = {
+  Github,
+  Twitter,
+  Instagram,
+}
+
+export default Icons
---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,18 @@
+// Objeto ou array que tem informações referentes do componentes em questão
+const handle = "jvsmartins"
+const links = [
+  {
+    label: "Github",
+    url: `https://github.com/${handle}`,
+  },
+  {
+    label: "Twitter",
+    url: `https://twitter.com/${handle}`,
+  },
+  {
+    label: "Instagram",
+    url: `https://www.instagram.com/${handle}`,
+  },
+]
+
+export default links
