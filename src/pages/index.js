7e5961e54be346fb4e6542192a01fd488eaa22f4
Joao Vitor Martins <j.martins@aluno.ifsp.edu.br>
2021-02-19 23:00:26 -0300

    Desenvolvimento dos componentes: Profile, SideBar e PostItem

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,22 @@
+//TODO: aula 5.14
 import React from "react"
 
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
+import PostItem from "../components/PostItem"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Home 1</h1>
+    <PostItem
+      slug="/about/"
+      background="blue"
+      category="Misc"
+      date="20 de Fevereiro de 2021"
+      timeToRead="3"
+      title="Diga sim ao Medium: tenha sua própria plataforma"
+      description="Algumas razões para você ter sua própria plataforma ao invés de soluções como o Medium"
+    />
   </Layout>
 )
 
