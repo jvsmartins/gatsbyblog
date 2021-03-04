4dc7c809f7b19698d3d1e09506170c9d23800eaf
Joao Vitor Martins <j.martins@aluno.ifsp.edu.br>
2021-03-03 21:40:41 -0300

    Componente de Paginação criado

--- src/templates/blog-list.js
+++ src/templates/blog-list.js
@@ -4,9 +4,18 @@ import { graphql } from "gatsby"
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
 import PostItem from "../components/PostItem"
+import Pagination from "../components/Pagination"
 
 const BlogList = props => {
   const postList = props.data.allMarkdownRemark.edges
+
+  //Puxando os valores da Página Atual e numero de paginas do gatsby-node na no context
+  const { currentPage, numPages } = props.pageContext
+
+  const isFirst = currentPage === 1
+  const isLast = currentPage === numPages
+  const prevPage = currentPage - 1 === 1 ? "/" : `/page/${currentPage - 1}`
+  const nextPage = `/page/${currentPage + 1}`
 
   return (
     <Layout>
@@ -30,6 +39,15 @@ const BlogList = props => {
           />
         )
       )}
+
+      <Pagination
+        isFirst={isFirst}
+        isLast={isLast}
+        currentPage={currentPage}
+        numPages={numPages}
+        prevPage={prevPage}
+        nextPage={nextPage}
+      />
     </Layout>
   )
 }
