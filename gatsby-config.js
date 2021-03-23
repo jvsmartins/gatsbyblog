f0cee82937763e4cdce216dfd2f2f5169a6547db
Joao Vitor Martins <j.martins@aluno.ifsp.edu.br>
2021-03-22 22:56:33 -0300

    Instalação e configuração inicial do plugin Algolia

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,9 @@
+const path = require("path")
+require("dotenv").config({ path: path.resolve(__dirname, ".env") })
+const queries = require("./src/utils/algolia_queries")
+
+console.log(process.env)
+
 module.exports = {
   siteMetadata: {
     title: `Obrigado pelo acesso e bem vindo ao meu blog`,
@@ -63,6 +69,17 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-algolia-search`,
+      options: {
+        appId: process.env.ALGOLIA_APP_ID,
+        apiKey: process.env.ALGOLIA_ADMIN_KEY,
+        indexName: process.env.ALGOLIA_INDEX_NAME,
+        queries,
+        chunkSize: 10000,
+        enablePartialUpdates: true, //default: false
+      },
+    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
